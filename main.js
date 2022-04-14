1ea36e1853470e2b37e377a5224a83bfa2409873
uhunogoo <uhonogoruk@gmail.com>
2022-04-14 21:32:50 +0300

    minor change for test

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ import Experience from './js/Experience/Experience'
 
 const canvas = document.querySelector('canvas.webgl')
 
-
 window.addEventListener("load", () => {
     const experiance = new Experience( canvas )
 });
