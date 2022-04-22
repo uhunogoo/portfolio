e2e030d31c12f0affc7802d07ebf4d6b7e70b89d
uhunogoo <uhonogoruk@gmail.com>
2022-04-22 17:33:17 +0300

    change stones to magic

--- js/Experience/Preload.js
+++ js/Experience/Preload.js
@@ -1,42 +1,65 @@
 import gsap from 'gsap'
 import Experience from './Experience'
+import EventEmitter from './Utils/EventEmitter'
 
-export default class Preload {
+export default class Preload extends EventEmitter {
     constructor() {
+        super()
+
         // Options
         this.experience = new Experience()
         this.resources = this.experience.resources
 
         // Setup
-        this.progress = { value: 0 }
+        this.progress = { value: 0, complete: false }
         this.progressBlock = document.querySelector('.loader span')
         
         // action
         this.resources.on('loadingProgress', () => {
             this.loading()
         })
-        this.resources.on('ready', () => {
-            this.load()
-        })
     }
     loading() {
         const that = this
-        // experience
-        gsap.to( this.progress, {
-            value: (this.resources.loaded / this.resources.toLoad),
-            duration: 1,
-            
-            onUpdate: animationUpdate
+        // Set default styles
+        gsap.set('.loader', {
+            clipPath: 'circle(150% at 50% 50%)'
         })
+
+        // experience
+        gsap.fromTo( this.progress, {value: this.progress.value}, {
+            value: (this.resources.loaded / this.resources.toLoad),
+            duration: 2,
+            onUpdate: animationUpdate,
+            onComplete: animationComplete
+        })
+        // Animate percent value
         function animationUpdate() {
             const v = 100 * this.progress().toFixed(2)
             that.progressBlock.innerHTML = v.toFixed(0)
         }
-    }
-    load() {
-        gsap.to( '.loader', {
-            delay: 2,
-            autoAlpha: true  
-        })
+        // Animation onComplete
+        function animationComplete() {
+            const outTL = gsap.timeline({
+                onComplete: () => {
+                    that.trigger('ready')
+                },
+                defaults: {
+                    duration: 0.8,
+                    ease: 'sine'
+                }
+            })
+            outTL.to( '.loader', {
+                delay: 0.5,
+                clipPath: 'circle(0% at 50% 50%)',
+            })
+            outTL.to('.loader span', {
+                filter: 'blur(1em)',
+                scale: 2
+            }, '<+=40%')
+            outTL.to('.loader', {
+                autoAlpha: true
+            })
+        }
     }
 }
--- js/Experience/World/Stones.js
+++ js/Experience/World/Stones.js
@@ -11,81 +11,30 @@ export default class Stones {
         this.debug = this.experience.debug
 
         // Setup
-        this.stonesParameters = {
-            count: 5,
-            radius: 0.6,
+        this.parameters = {
+            radius: 2,
             theta: Math.PI * 0.5,
             yPosition: 2
         }
-        this.parameters = {}
 
         // Debug
         if (this.debug.active) {
             this.debugFolder = this.debug.ui.addFolder('Stones')
             // this.debugFolder.close()
         }
-        this.generateCircularShape()
-        this.createStones()
-
-        this.animation()
+        this.generateShape()
     }
-    generateCircularShape() {
-        const { radius, theta, count } = this.stonesParameters
-        this.circleShape = new THREE.CircleGeometry( radius, count, theta )
-        this.stepAngle = (Math.PI * 2) / count
-    }
-    createStones() {
-        this.parameters.color = 0x2d2d2d
-        this.stonesGroup = new THREE.Group()
-
-        const stoneGeometry = this.resources.items.stoneModel.scene.children.find( child => child.name === 'stone').geometry
-        const stoneMaterial = new THREE.MeshStandardMaterial({ color: this.parameters.color })
-
-        const {count, yPosition} = this.stonesParameters
-    
-        // base geometry parameters
-        const positions = this.circleShape.getAttribute('position').array
-    
-        // Create geometry on each circle vertices
-        // Start from 1 because 0 is center
-        for (let i = 1; i <= count; i++) {
-            const i3 = i * 3
-            
-            const geometry = stoneGeometry.clone()
-            const material = stoneMaterial.clone()
-            const stoneMesh = new THREE.Mesh( geometry, material )
-
-            // aply positions but flip z and y values
-            stoneMesh.position.set(
-                positions[i3 + 0] * -1,
-                positions[i3 + 2],
-                positions[i3 + 1]
-            )
-            stoneMesh.rotation.y = this.stepAngle * (i - 1)
-            stoneMesh.scale.set(0.12, 0.12, 0.12)
-
-            this.stonesGroup.add( stoneMesh )
-
-        }
-        this.stonesGroup.position.y = yPosition
-        this.scene.add( this.stonesGroup )
-
-        // Debug shader material
-        if (this.debug.active) {
-            this.debugFolder.addColor( this.parameters, 'color').name('stonesColor').onChange( () => {
-                this.stonesGroup.children.forEach(stone => stone.material.color.set(this.parameters.color) )
-            })
-        }
-    }
-    animation() {
-        gsap.to(this.stonesGroup.rotation, {
-            y: Math.PI *2,
-            duration: 2,
-            ease: 'none',
-            repeat: -1
-        })
+    generateShape() {
+        const shape = new THREE.PlaneBufferGeometry( this.parameters.radius, this.parameters.radius )
+        const shapeMaterial = new THREE.MeshBasicMaterial({ side: THREE.DoubleSide })
         
-        
+        const mesh = new THREE.Mesh( shape, shapeMaterial )
+        mesh.position.y = this.parameters.yPosition
+        mesh.rotation.x = Math.PI * 0.5
+
+        this.scene.add(mesh)
+    }
+    animation() { 
         gsap.to(this.stonesGroup.position, {
             keyframes:{
                 "50%":{y: '+=0.05', ease:"sine"},
