e0fc49995018c7a3a5d890814f9a7175924ca7d5
uhunogoo <uhonogoruk@gmail.com>
2022-05-13 02:19:56 +0300

    change points rendering to css2D render

--- js/Experience/Animations/PointsCalculation.js
+++ js/Experience/Animations/PointsCalculation.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three'
-
+import { CSS2DRenderer, CSS2DObject } from 'three/examples/jsm/renderers/CSS2DRenderer.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import Experience from '../Experience'
 
 export default class pointsOfInterest {
@@ -24,46 +25,66 @@ export default class pointsOfInterest {
         this.animationComplete = false
         this.raycaster = new THREE.Raycaster()
 
-
-        // var targetPosition = new THREE.Vector3();
-        // targetPosition = targetPosition.setFromMatrixPosition( cube.matrixWorld );
-
-        // var lookAt = testCamera.getWorldDirection();
-        // var cameraPos = new THREE.Vector3().setFromMatrixPosition( testCamera.matrixWorld );
-        // var pos = targetPosition.sub( cameraPos );
-
-        // behind = ( pos.angleTo( lookAt ) ) > ( Math.PI / 2 );
-
-
-        console.log(this.points[0].position)
+        this.createPoints()
     }
+
+    createPoints() {
+        const geometry = new THREE.BoxBufferGeometry(0, 0, 0)
+        const material = new THREE.MeshBasicMaterial()
+        this.points.forEach( point => {
+            const mesh = new THREE.Mesh(
+                geometry.clone(),
+                material.clone()
+            )
+            mesh.position.copy( point.position )
+            this.scene.add(mesh)
+
+            const pointLabel = new CSS2DObject( point.element )
+            pointLabel.position.set(0, 0, 0 )
+            mesh.add( pointLabel )
+
+
+            this.labelRenderer = new CSS2DRenderer()
+            this.labelRenderer.setSize( window.innerWidth, window.innerHeight )
+            this.labelRenderer.domElement.style.position = 'absolute'
+            this.labelRenderer.domElement.style.top = '0px'
+            document.body.appendChild( this.labelRenderer.domElement )
+
+            this.controls = new OrbitControls( this.camera, this.labelRenderer.domElement )
+            this.controls.enableDamping = true
+            this.controls.maxPolarAngle = Math.PI * 0.53
+        })
+    }
+
     update() {
-        for (const point of this.points ) {
-            const screenPosition = point.position.clone()
-            screenPosition.project(this.camera)
+        this.controls.update()
+        this.labelRenderer.render( this.scene, this.camera );
+        // for (const point of this.points ) {
+        //     const screenPosition = point.position.clone()
+        //     screenPosition.project(this.camera)
 
-            // console.log( this.camera.position.angleTo( point.position ) > ( Math.PI / 2 ) )
+        //     // console.log( this.camera.position.angleTo( point.position ) > ( Math.PI / 2 ) )
             
-            const pointInView = (Math.abs(screenPosition.x) >= 1 || Math.abs(screenPosition.y) >= 1) ? false : true
+        //     const pointInView = (Math.abs(screenPosition.x) >= 1 || Math.abs(screenPosition.y) >= 1) ? false : true
 
-            this.raycaster.setFromCamera( screenPosition, this.camera )
-            const intersects = this.raycaster.intersectObjects(this.scene.children, true)
-            if (intersects.length === 0) {
-                point.element.classList.add('visible')
-            } else {
-                const intersectionDistance = intersects[0].distance
-                const pointDistance = point.position.distanceTo(this.camera.position)
-                if ( intersectionDistance < pointDistance || !pointInView ) {
-                    point.element.classList.remove('visible')
-                } else {
-                    point.element.classList.add('visible') 
-                }
-            }
+        //     this.raycaster.setFromCamera( screenPosition, this.camera )
+        //     const intersects = this.raycaster.intersectObjects(this.scene.children, true)
+        //     if (intersects.length === 0) {
+        //         point.element.classList.add('visible')
+        //     } else {
+        //         const intersectionDistance = intersects[0].distance
+        //         const pointDistance = point.position.distanceTo(this.camera.position)
+        //         if ( intersectionDistance < pointDistance || !pointInView ) {
+        //             point.element.classList.remove('visible')
+        //         } else {
+        //             point.element.classList.add('visible') 
+        //         }
+        //     }
 
-            const translateX = screenPosition.x * this.sizes.width * 0.5
-            const translateY = -screenPosition.y * this.sizes.height * 0.5
-            point.element.style.transform = `translate(${Math.trunc(translateX)}px, ${Math.trunc(translateY)}px)`
+        //     // const translateX = screenPosition.x * this.sizes.width * 0.5
+        //     // const translateY = -screenPosition.y * this.sizes.height * 0.5
+        //     // point.element.style.transform = `translate(${Math.trunc(translateX)}px, ${Math.trunc(translateY)}px)`
 
-        }
+        // }
     }
 }
--- js/Experience/Utils/Points.js
+++ js/Experience/Utils/Points.js
@@ -3,14 +3,14 @@ export default class Points {
     constructor() {
         // Setup
         this.list = [
-            // {
-            //     element: document.querySelector('.point-0'),
-            //     position: new THREE.Vector3( 0, 1, 0 )
-            // },
-            // {
-            //     element: document.querySelector('.point-1'),
-            //     position: new THREE.Vector3( 0, 3.2, 0 )
-            // },
+            {
+                element: document.querySelector('.point-0'),
+                position: new THREE.Vector3( 0, 1, 0 )
+            },
+            {
+                element: document.querySelector('.point-1'),
+                position: new THREE.Vector3( 0, 3.2, 0 )
+            },
             {
                 element: document.querySelector('.point-2'),
                 position: new THREE.Vector3( 3, 0.3, -2 )
