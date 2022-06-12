d9c54260fbf4d91b4555f5a8ee5ff0baac9bcc9d
uhunogoo <uhonogoruk@gmail.com>
2022-06-12 03:05:46 +0300

    cleanup animation

--- js/Experience/Animations/PointsAnimtion.js
+++ js/Experience/Animations/PointsAnimtion.js
@@ -61,7 +61,7 @@ export default class PointsAnimation extends EventEmitter {
             }
         })
         gsap.registerEffect({
-            name: "pointInfocontainer",
+            name: "openInformationBlock",
             extendTimeline:true,
             effect: (target, parameters) => {                
                 const tl = gsap.timeline({
@@ -109,9 +109,9 @@ export default class PointsAnimation extends EventEmitter {
         })
 
         this.showNav()
-        this.closeBtn()
-        this.navigationBtn() 
         this.startStyles()
+        
+        this.clickHandler()
     }
     startStyles() {
         gsap.set('.work', 
@@ -120,9 +120,13 @@ export default class PointsAnimation extends EventEmitter {
             y: '180%',
             opacity: 0
         })
-        gsap.set( '#myPhoto', { scale: 1.4, yPercent: -120 })
+        // gsap.set( '#myPhoto', { clipPath: 'inset(100% 0% 0% 0%)' }) // to top
+        gsap.set( '#myPhoto', { clipPath: 'inset(0% 0% 100% 0%)' }) // to bottom
+        gsap.set( '#myPhoto img', { scale: 1.4 })
         gsap.set( '.content__title span', { yPercent: 100 })
-        gsap.set( '.content__text p', { opacity: 0 })
+        gsap.set( '.animate-text', { opacity: 0, y: 100 })
+        gsap.set( '.content__text .icon', { opacity: 0, scale: 0.1 })
+        gsap.set( '.content__links', { opacity: 0, scale: 0.1 })
     }
     showNav() {
         this.showPoints = gsap.timeline({ paused: true })        
@@ -135,26 +139,7 @@ export default class PointsAnimation extends EventEmitter {
             stagger: 0.2
         })
     }
-    closeBtn() {
-        const playHitSound = () => {
-            this.outSound.currentTime = 0
-            this.outSound.play()
-        }
-        if( this.pointInfoOpen ) {
-            const closeBtn = document.querySelector('.close_btn')
-            closeBtn.addEventListener('click', () => {
-                this.closeButtonClicked = true
-                this.trigger('menuWasClose')
-
-                playHitSound()
-                this.open.reverse()
-                this.pointInfoOpen = false
-            })
-        }
-    }
-    towerAnimation(target) {
-        console.log( target )
-        
+    towerAnimation(target) {        
         const tl = gsap.timeline()
         tl.to(this.pointsScale, {
             x: 0,
@@ -182,35 +167,57 @@ export default class PointsAnimation extends EventEmitter {
 
         return tl
     }
-    descriptionBlockAnimation( target, sound ) {
-        const tl = gsap.timeline({
-            onStart: sound
+    
+    clickHandler() {
+        const playHitSound = () => {
+            this.outSound.currentTime = 0
+            this.outSound.play()
+        }
+        const openInformationBlock = (targetPoint) => {
+            this.trigger('menuWasOpen')
+            this.showInformation(targetPoint)
+        }
+        document.addEventListener('click', (e) => {
+            const clicked = e.target
+            // Click targets
+            const clickOnCanvas = clicked.classList.contains('webgl')
+            const clickOnCloseButton = clicked.classList.contains('close_btn')
+            const clickOnNavigationButton = clicked.classList.contains('menu__item')
+
+            if ( clickOnCanvas ) {
+                if (this.intersect && !this.pointInfoOpen) {
+                    this.pointInfoOpen = true
+                    const targetPoint = this.points.find( point => point.position.equals( this.intersect.position ) )
+                    
+                    openInformationBlock(targetPoint)
+                }
+            } 
+            if ( clickOnCloseButton ) {
+                if( this.pointInfoOpen ) {
+                    this.closeButtonClicked = true
+                    this.trigger('menuWasClose')
+        
+                    playHitSound()
+                    this.open.reverse()
+                    this.pointInfoOpen = false
+                }
+            }
+            if(clickOnNavigationButton) {
+                if (!this.pointInfoOpen) {
+                    this.pointInfoOpen = true
+
+                    const name = clicked.dataset.name
+                    const targetPoint = this.points.find( point => point.name === name )
+                    
+                    openInformationBlock(targetPoint)
+                }
+            }         
         })
-        tl.to(this.preload.material.uniforms.uProgress, {
-            value: 1,
-            duration: 1.6,
-            ease: 'power3'
-        })
-        tl.to([ '.informationPart', target.element], {
-            autoAlpha: 1,
-            duration: 0.1
-        }, '<')
-        tl.to('.work', {
-            clipPath: 'inset(0% round 15px)',
-            y: 0,
-            opacity: 1,
-            duration: 1.2,
-            stagger: 0.1,
-        }, '<+=25%')
-        tl.to('.work__image', {
-            scale: 1.1,
-            stagger: 0.07,
-            duration: 1.25,
-            ease: 'power2'
-        }, '<')
-        return tl
+
+        
     }
-    openMenu(target) {
+
+    showInformation(target) {
         const playHitSound = () => {
             this.hitSound.currentTime = 0
             this.hitSound.play()
@@ -218,22 +225,105 @@ export default class PointsAnimation extends EventEmitter {
         this.open = gsap.timeline({ paused: true })
 
         const parameters = {}
-        if (target.name === 'point-1') {   
-            parameters.function = this.descriptionBlockAnimation(target, playHitSound).timeScale(2.6).pause()
-        } else if (target.name === 'point-2') {   
-            parameters.function = this.aboutMeAnimation(target, playHitSound).timeScale(1.6).pause()
-        }
-        this.open.pointInfocontainer( target, parameters )
+        parameters.function = this.informationBlockAnimation(target, playHitSound)
+
+        this.open.openInformationBlock( target, parameters )
         this.open.play()
     }
-    clickHandler(target) {
-        this.trigger('menuWasOpen')
-        this.openMenu(target)
 
-        if(!this.closeButtonClicked) {
-            this.closeBtn()
+    /**
+     * 
+     * @param {*} target 
+     * @param {*} sound 
+     * @returns timeline
+     */
+    informationBlockAnimation(target, sound) {
+        const tl = gsap.timeline({ paused: true, onStart: sound })
+
+        tl.to(this.preload.material.uniforms.uProgress, {
+            value: 1,
+            duration: 1,
+            ease: 'power3'
+        })
+        tl.to([ '.informationPart', target.element], {
+            autoAlpha: 1,
+            duration: 0.1
+        }, '<')
+
+        if (target.name === 'point-1') {
+            // Open my works
+            tl.add( this.myWorksBlockAnimation().timeScale(2.6), '<+=10%' )
+        } else if (target.name === 'point-2') {
+            // Open about me
+            tl.add( this.aboutMeAnimation().timeScale(1.6), '<+=10%' )
         }
+
+        return tl
     }
+    myWorksBlockAnimation() {
+        const tl = gsap.timeline()
+
+        tl.to('.work', {
+            clipPath: 'inset(0% round 15px)',
+            y: 0,
+            opacity: 1,
+            duration: 1.2,
+            stagger: 0.1,
+        })
+        tl.to('.work__image', {
+            scale: 1.1,
+            stagger: 0.07,
+            duration: 1.25,
+            ease: 'power2'
+        }, '<')
+
+        return tl
+    }
+    aboutMeAnimation() {
+        const tl = gsap.timeline()
+        tl.to('#myPhoto', {
+            opacity: 1,
+            clipPath: 'inset(0% 0% 0% 0%)',
+            duration: 1.1,
+            ease: 'power4'
+        })
+        tl.to('#myPhoto img', {
+            scale: 1,
+            duration: 1,
+            ease: 'power1'
+        }, '<')
+        tl.to('.content__title span', {
+            yPercent: 0,
+            stagger: {
+                amount: 0.4,
+                ease: 'power2'
+            }
+        }, '<')
+        tl.to('.animate-text', {
+            opacity: 1,
+            y: 0,
+            stagger: {
+                amount: 0.5,
+                ease: 'power3'
+            }
+        }, '<')
+        tl.to('.content__text .icon', {
+            opacity: 1,
+            scale: 1,
+            stagger: {
+                amount: 0.4,
+                ease: 'power3.in'
+            }
+        }, '<+=50%')
+        tl.to('.content__links', {
+            opacity: 1,
+            scale: 1,
+            ease: 'power3.in'
+        }, '<-=50%')
+        
+        return tl
+    }
+    
     clean() {
         if (this.intersect) {
             this.tl.kill()
@@ -243,55 +333,6 @@ export default class PointsAnimation extends EventEmitter {
 
             this.intersect = null
         }
-    }
-
-    aboutMeAnimation(target, sound) {
-        const tl = gsap.timeline({ onStart: sound })
-
-        tl.to(this.preload.material.uniforms.uProgress, {
-            value: 1,
-            duration: 1.6,
-            ease: 'power3'
-        })
-        tl.to([ '.informationPart', target.element], {
-            autoAlpha: 1,
-            duration: 0.1
-        }, '<')
-        tl.to('#myPhoto', {
-            scale: 1,
-            yPercent: 0,
-            duration: 1,
-            ease: 'power2'
-        }, '<+=50%')
-        tl.to('.content__title span', {
-            yPercent: 0,
-            stagger: {
-                amount: 0.4,
-                ease: 'power2'
-            }
-        }, '<')
-        tl.to('.content__text p', {
-            opacity: 1,
-            stagger: {
-                amount: 0.7,
-                ease: 'power2'
-            }
-        }, '<')
-        return tl
-    }
-    
-    navigationBtn() {
-        const navigationBtn = document.querySelectorAll('.menu__item')
-        navigationBtn.forEach( button => {
-            button.addEventListener('click', () => {                
-                if (!this.pointInfoOpen) {
-                    this.pointInfoOpen = true
-                    const name = button.dataset.name
-                    const targetPoint = this.points.find( point => point.name === name )
-                    this.clickHandler( targetPoint )
-                }
-            })
-        })
     }
     raycasterAnimation() {
         const mouse = new THREE.Vector2( this.mouse.x, this.mouse.y )
@@ -311,13 +352,6 @@ export default class PointsAnimation extends EventEmitter {
                 
                 this.tl = gsap.timeline()
                 this.tl.pointsShow( this.intersect.scale, {x: 0.2, y: 0.2} )
-                document.addEventListener('click', () => {                    
-                    if (this.intersect && !this.pointInfoOpen) {
-                        this.pointInfoOpen = true
-                        const targetPoint = this.points.find( point => point.position.equals( this.intersect.position ) )
-                        this.clickHandler( targetPoint )
-                    }
-                })
             }
         } else {
             // Cursor default
