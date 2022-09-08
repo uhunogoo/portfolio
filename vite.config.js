933e6e91ba1efbad042bc62882c3269d4a03e7c0
uhunogoo <uhonogoruk@gmail.com>
2022-09-08 18:45:48 +0300

    add minification for html

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,28 @@
 import basicSsl from '@vitejs/plugin-basic-ssl'
+import { createHtmlPlugin } from 'vite-plugin-html'
+// import { defineConfig } from 'vite'
 /**
 * @type {import('vite').UserConfig}
 */
 
 export default {
     build: {
+        minify: 'terser',
         cssCodeSplit: false,
         sourcemap: true,
     },
+        // loader
+        
+    // },
     server: {
         https: true,
         host: '192.168.31.122',
         port: '3000'
     },
     plugins: [
-        basicSsl()
+        basicSsl(),
+        createHtmlPlugin({
+            minify: true,
+        })
     ]
 }
